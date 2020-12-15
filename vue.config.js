21b1699ea6b6a22a26a8320b6a779599493135c6
Nicke Manarin <nicke@outlook.com.br>
2020-12-15 10:20:33 -0300

    Test of electron-builder.

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/Franchise-Map/" : "/", 
+    publicPath: process.env.NODE_ENV === "production" ? "/Franchise-Map/" : "/",
 
     chainWebpack: config => {
         config.module
-          .rule('raw')
-          .test(/\.csv$/)
-          .use('raw-loader')
-          .loader('raw-loader')
-          .end()
+            .rule('raw')
+            .test(/\.csv$/)
+            .use('raw-loader')
+            .loader('raw-loader')
+            .end()
+    },
+
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                win: {
+                    target: 'portable'
+                },
+            }
+        }
     }
 }
